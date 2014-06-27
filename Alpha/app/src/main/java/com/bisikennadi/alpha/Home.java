86234b5f5647c5310d57f922374e358f6f09ff08
Bisike Nnadi <bisike@thinkagency.com>
2014-06-27 16:24:59 -0400

    working progress

--- Alpha/app/src/main/java/com/bisikennadi/alpha/Home.java
+++ Alpha/app/src/main/java/com/bisikennadi/alpha/Home.java
@@ -66,94 +66,94 @@ public class Home extends Activity {
     }
 
     public void nextPage(View view){
-        switch (view.getId()) {
-            case R.id.myProjects_btn:
-                Intent projIntent = new Intent(this, TempList.class);
-                page = "My Projects";
-                projIntent.putExtra(PAGE_NAME, page);
-                startActivity(projIntent);
-                break;
-            case R.id.estimates_btn:
-                Intent estIntent = new Intent(this, TempList.class);
-                page = "Estimates";
-                estIntent.putExtra(PAGE_NAME, page);
-                startActivity(estIntent);
-                break;
-            case R.id.presentation_btn:
-                Intent presIntent = new Intent(this, TempList.class);
-                page = "Presentation Builder";
-                presIntent.putExtra(PAGE_NAME, page);
-                startActivity(presIntent);
-                break;
-            case R.id.productLit_btn:
-                Intent prodIntent = new Intent(this, TempList.class);
-                page = "Product Literature";
-                prodIntent.putExtra(PAGE_NAME, page);
-                startActivity(prodIntent);
-                break;
-            case R.id.shingle_btn:
-                Intent shinIntent = new Intent(this, TempList.class);
-                page = "Shingle Visualization";
-                shinIntent.putExtra(PAGE_NAME, page);
-                startActivity(shinIntent);
-                break;
-            case R.id.eagleView_btn:
-                Intent eagleIntent = new Intent(this, TempList.class);
-                page = "Eagle View";
-                eagleIntent.putExtra(PAGE_NAME, page);
-                startActivity(eagleIntent);
-                break;
-            case R.id.atlas_store_btn:
-                Intent storeIntent = new Intent(this, TempList.class);
-                page = "Atlas Store";
-                storeIntent.putExtra(PAGE_NAME, page);
-                startActivity(storeIntent);
-                break;
-            case R.id.locateDist_btn:
-                Intent distIntent = new Intent(this, TempList.class);
-                page = "Locate A Distributor";
-                distIntent.putExtra(PAGE_NAME, page);
-                startActivity(distIntent);
-                break;
-            case R.id.warranty_btn:
-                Intent warrIntent = new Intent(this, TempList.class);
-                page = "Warranty Submissions";
-                warrIntent.putExtra(PAGE_NAME, page);
-                startActivity(warrIntent);
-                break;
-            case R.id.atlas_rebates_btn:
-                Intent rebateIntent = new Intent(this, TempList.class);
-                page = "Atlas Rebates";
-                rebateIntent.putExtra(PAGE_NAME, page);
-                startActivity(rebateIntent);
-                break;
-            case R.id.homeowner_lead_btn:
-                Intent homeIntent = new Intent(this, TempList.class);
-                page = "Homeowners Leads";
-                homeIntent.putExtra(PAGE_NAME, page);
-                startActivity(homeIntent);
-                break;
-            case R.id.storm_track_btn:
-                Intent stormIntent = new Intent(this, TempList.class);
-                page = "Storm Tracking";
-                stormIntent.putExtra(PAGE_NAME, page);
-                startActivity(stormIntent);
-                break;
-            case R.id.testimonials_btn:
-                Intent testIntent = new Intent(this, TempList.class);
-                page = "Testimonials";
-                testIntent.putExtra(PAGE_NAME, page);
-                startActivity(testIntent);
-                break;
-            case R.id.social_share_btn:
-                Intent socialIntent = new Intent(this, TempList.class);
-                page = "Social Sharing";
-                socialIntent.putExtra(PAGE_NAME, page);
-                startActivity(socialIntent);
-                break;
-
-
-
-        }
+//        switch (view.getId()) {
+//            case R.id.myProjects_btn:
+//                Intent projIntent = new Intent(this, TempList.class);
+//                page = "My Projects";
+//                projIntent.putExtra(PAGE_NAME, page);
+//                startActivity(projIntent);
+//                break;
+//            case R.id.estimates_btn:
+//                Intent estIntent = new Intent(this, TempList.class);
+//                page = "Estimates";
+//                estIntent.putExtra(PAGE_NAME, page);
+//                startActivity(estIntent);
+//                break;
+//            case R.id.presentation_btn:
+//                Intent presIntent = new Intent(this, TempList.class);
+//                page = "Presentation Builder";
+//                presIntent.putExtra(PAGE_NAME, page);
+//                startActivity(presIntent);
+//                break;
+//            case R.id.productLit_btn:
+//                Intent prodIntent = new Intent(this, TempList.class);
+//                page = "Product Literature";
+//                prodIntent.putExtra(PAGE_NAME, page);
+//                startActivity(prodIntent);
+//                break;
+//            case R.id.shingle_btn:
+//                Intent shinIntent = new Intent(this, TempList.class);
+//                page = "Shingle Visualization";
+//                shinIntent.putExtra(PAGE_NAME, page);
+//                startActivity(shinIntent);
+//                break;
+//            case R.id.eagleView_btn:
+//                Intent eagleIntent = new Intent(this, TempList.class);
+//                page = "Eagle View";
+//                eagleIntent.putExtra(PAGE_NAME, page);
+//                startActivity(eagleIntent);
+//                break;
+//            case R.id.atlas_store_btn:
+//                Intent storeIntent = new Intent(this, TempList.class);
+//                page = "Atlas Store";
+//                storeIntent.putExtra(PAGE_NAME, page);
+//                startActivity(storeIntent);
+//                break;
+//            case R.id.locateDist_btn:
+//                Intent distIntent = new Intent(this, TempList.class);
+//                page = "Locate A Distributor";
+//                distIntent.putExtra(PAGE_NAME, page);
+//                startActivity(distIntent);
+//                break;
+//            case R.id.warranty_btn:
+//                Intent warrIntent = new Intent(this, TempList.class);
+//                page = "Warranty Submissions";
+//                warrIntent.putExtra(PAGE_NAME, page);
+//                startActivity(warrIntent);
+//                break;
+//            case R.id.atlas_rebates_btn:
+//                Intent rebateIntent = new Intent(this, TempList.class);
+//                page = "Atlas Rebates";
+//                rebateIntent.putExtra(PAGE_NAME, page);
+//                startActivity(rebateIntent);
+//                break;
+//            case R.id.homeowner_lead_btn:
+//                Intent homeIntent = new Intent(this, TempList.class);
+//                page = "Homeowners Leads";
+//                homeIntent.putExtra(PAGE_NAME, page);
+//                startActivity(homeIntent);
+//                break;
+//            case R.id.storm_track_btn:
+//                Intent stormIntent = new Intent(this, TempList.class);
+//                page = "Storm Tracking";
+//                stormIntent.putExtra(PAGE_NAME, page);
+//                startActivity(stormIntent);
+//                break;
+//            case R.id.testimonials_btn:
+//                Intent testIntent = new Intent(this, TempList.class);
+//                page = "Testimonials";
+//                testIntent.putExtra(PAGE_NAME, page);
+//                startActivity(testIntent);
+//                break;
+//            case R.id.social_share_btn:
+//                Intent socialIntent = new Intent(this, TempList.class);
+//                page = "Social Sharing";
+//                socialIntent.putExtra(PAGE_NAME, page);
+//                startActivity(socialIntent);
+//                break;
+//
+//
+//
+//        }
     }
 }
